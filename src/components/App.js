8d3b0bfa7ed4ea36ed8c9370ee9367a914ca9dd0
jaynson <lyghtshades@outlook.com>
2021-11-04 06:50:09 -0800

    Initial HTML markup

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import CalculatorView from './CalculatorView';
+import DisplayPanelView from './DisplayPanelView';
 
 
 const App = () => {
     return (
         <div className='container'>
             App
+            <CalculatorView />
+            <DisplayPanelView />
         </div>
     );
 };
