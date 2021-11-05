9a09005e4715baaad672432e8c3dd3d306d96ef1
jaynson <lyghtshades@outlook.com>
2021-11-05 10:41:42 -0800

    Responsive Design added

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useState } from 'react/cjs/react.development';
+import React, { useEffect, useState } from 'react';
 import CalculatorView from './CalculatorView';
 import DisplayPanelView from './DisplayPanelView';
 
@@ -12,12 +11,38 @@ const App = () => {
         }
     );
 
+    const [reset, setReset] = useState(false);
+    useEffect(() => {
+        console.log(reset);
+
+        return () => {
+            setReset(false);
+        };
+    }, [reset]);
+
+    const [empty, setEmpty] = useState(true);
+
     return (
         <div className='container'>
             <div className='component'>
+                <svg className='svg__logo'>
+                    <use xlinkHref={ `sprite.svg#logo` }></use>
+                </svg>
                 <div className='app'>
-                    <CalculatorView calculate={ setResults } />
-                    <DisplayPanelView results={ results } />
+                    <CalculatorView
+                        calculate={ setResults }
+                        resetView={ setReset }
+                        reset={ reset }
+                        emptyView={ setEmpty }
+                        empty={ empty }
+                    />
+                    <DisplayPanelView
+                        results={ results }
+                        resetView={ setReset }
+                        reset={ reset }
+                        emptyView={ setEmpty }
+                        empty={ empty }
+                    />
                 </div>
             </div>
         </div>
