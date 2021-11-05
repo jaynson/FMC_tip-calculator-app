48f3ea40bae13f5d9a0e0b02bcf11624c2144f2c
jaynson <lyghtshades@outlook.com>
2021-11-05 02:15:15 -0800

    Major Styling and Functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import { useState } from 'react/cjs/react.development';
 import CalculatorView from './CalculatorView';
 import DisplayPanelView from './DisplayPanelView';
 
 
 const App = () => {
+    const [results, setResults] = useState(
+        {
+            tip: 0.00,
+            total: 0.00
+        }
+    );
+
     return (
         <div className='container'>
-            App
-            <CalculatorView />
-            <DisplayPanelView />
+            <div className='component'>
+                <div className='app'>
+                    <CalculatorView calculate={ setResults } />
+                    <DisplayPanelView results={ results } />
+                </div>
+            </div>
         </div>
     );
 };
